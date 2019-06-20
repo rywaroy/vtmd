e93c55ebead3ec8fd07b2e094f2e3c66db50dec0
zhangzhihao <464485682@qq.com>
2019-06-20 17:18:34 +0800

    Feat

--- src/ast-parse.js
+++ src/ast-parse.js
@@ -1,7 +1,19 @@
 const babelParser = require('@babel/parser');
+const traverse = require('@babel/traverse').default;
 
 module.exports = function astParse(script) {
-  babelParser.parse(script, {
+  const ast = babelParser.parse(script, {
     sourceType: 'module',
+  });
+  traverse(ast, {
+    enter(path) {
+      if (path.node.leadingComments) {
+        console.log(path.node);
+        // console.log(path.node.leadingComments);
+      }
+    },
+    // ExportDefaultDeclaration(path) {
+    //   console.log(path.node);
+    // },
   });
 };
