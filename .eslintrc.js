595e4b7e9cdb5f2d37191d68b3a7f1ad8908efac
zhangzhihao <464485682@qq.com>
2019-06-21 17:04:38 +0800

    Feat

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
     rules: {
         'global-require': 0,
         'import/no-dynamic-require': 0,
-        'no-use-before-define': 0
+        'no-use-before-define': 0,
+        'arrow-parens': 0,
+        'prefer-destructuring': 0,
     }
 };
