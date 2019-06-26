44d05cfbba317b9d24f0f79fd1643013abe3f936
zhangzhihao <464485682@qq.com>
2019-06-26 17:05:52 +0800

    Feat

--- src/ast-parse.js
+++ src/ast-parse.js
@@ -252,6 +252,42 @@ function getData(props) {
         obj.value = filterComment(props[i + 1].leadingComments);
       }
     }
+    if (props[i].value.type === 'NumericLiteral') {
+      obj.type = 'Number';
+    }
+    if (props[i].value.type === 'StringLiteral') {
+      obj.type = 'String';
+    }
+    if (props[i].value.type === 'ArrayExpression') {
+      obj.type = 'Array';
+    }
+    if (props[i].value.type === 'ObjectExpression') {
+      obj.type = 'Object';
+      /**
+       * @example
+       * a: { // 判断这种情况的注释
+       *  b: 1,
+       * }
+       */
+      if (props[i].value.properties.length > 0) {
+        if (props[i].value.properties[0].leadingComments) {
+          obj.value = filterComment(props[i].value.properties[0].leadingComments);
+        }
+      } else {
+        /**
+       * @example
+       * a: { // 判断这种情况的注释
+        *
+        * }
+        */
+        if (props[i].value.innerComments) {
+          obj.value = filterComment(props[i].value.innerComments);
+        }
+      }
+    }
+    if (props[i].value.type === 'NullLiteral') {
+      obj.type = 'Any';
+    }
     arr.push(obj);
   }
   return arr;
