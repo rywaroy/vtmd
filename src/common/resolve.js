1b2675724a91e8c331a3bdd8926834e25bc28e1a
张志豪 <464485682@qq.com>
2019-10-09 11:11:53 +0800

    feat: 划分公共模块功能

---
+++ src/common/resolve.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const processPath = process.cwd();
+
+module.exports = function resolve(p) {
+  if (!p) {
+    return p;
+  }
+  if (Array.isArray(p)) {
+    return p.map(item => path.join(processPath, item));
+  }
+  return path.join(processPath, p);
+}
+;
