c33d3b383726b14334854e87c53c76381ca8bd66
张志豪 <464485682@qq.com>
2019-10-10 10:55:35 +0800

    Feat: 添加文件解析指定文件夹功能

--- src/umi/index.js
+++ src/umi/index.js
@@ -1,3 +1,15 @@
-module.exports = function createUmiDocument() {
+const path = require('path');
+const resolve = require('../common/resolve');
+const fileDisplay = require('./file-display');
 
+module.exports = function createUmiDocument(options) {
+  let umiFiles = [];
+
+  if (options.target.length > 0) {
+  // 配置过目标文件则解析目标文件
+    for (let i = 0; i < options.target.length; i++) {
+      umiFiles = umiFiles.concat(fileDisplay(resolve(options.target[i]), resolve(options.ignore)));
+    }
+  }
+  console.log(umiFiles);
 };
