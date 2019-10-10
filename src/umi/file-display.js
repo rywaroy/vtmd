513cea098a68448064eeedc4dc0577779f35c832
张志豪 <464485682@qq.com>
2019-10-10 11:17:14 +0800

    Feat: 文件夹递归添加components文件判断逻辑

--- src/umi/file-display.js
+++ src/umi/file-display.js
@@ -26,7 +26,10 @@ module.exports = function fileDisplay(filePaths, ignorePaths) {
             filesObj.models.push(path.join(filedir, item));
           });
         } else if (filename === 'components') {
-
+          const components = fs.readdirSync(filedir);
+          components.forEach(item => {
+            filesObj.components.push(path.join(filedir, item));
+          });
         } else {
           fileDisplayDeep(filedir); // 递归，如果是文件夹，就继续遍历该文件夹下面的文件
         }
