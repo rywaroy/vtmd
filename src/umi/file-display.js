3096d3dd7b71fe0f3ab6b03c72958760018985bc
张志豪 <464485682@qq.com>
2019-10-10 14:13:37 +0800

    Feat: 添加文件路径解析

--- src/umi/file-display.js
+++ src/umi/file-display.js
@@ -2,40 +2,54 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function fileDisplay(filePaths, ignorePaths) {
-  const umifiles = [];
-  function fileDisplayDeep(filePath) {
-    if (ignorePaths && ignorePaths.includes(filePath)) {
-      return;
-    }
-    const files = fs.readdirSync(filePath);
-    const filesObj = {
-      index: '',
-      map: '',
-      models: [],
-      components: [],
-    }; // 文件对象，包含index.js map.js model.js等
-    files.forEach((filename) => {
-      const filedir = path.join(filePath, filename); // 绝对路径
-      const stats = fs.statSync(filedir);
-      const isFile = stats.isFile(); // 是文件
-      const isDir = stats.isDirectory(); // 是文件夹
-      if (isDir) {
-        if (filename === 'models') { // 判断是models文件夹，遍历该文件夹
-          const models = fs.readdirSync(filedir);
-          models.forEach(item => {
-            filesObj.models.push(path.join(filedir, item));
-          });
-        } else if (filename === 'components') {
-          const components = fs.readdirSync(filedir);
-          components.forEach(item => {
-            filesObj.components.push(path.join(filedir, item));
-          });
-        } else {
-          fileDisplayDeep(filedir); // 递归，如果是文件夹，就继续遍历该文件夹下面的文件
+    const umifiles = [];
+    function fileDisplayDeep(filePath) {
+        if (ignorePaths && ignorePaths.includes(filePath)) {
+            return;
         }
-      }
-    });
-  }
-  fileDisplayDeep(filePaths);
-  return umifiles;
+        const files = fs.readdirSync(filePath);
+        const filesObj = {
+            index: '',
+            map: '',
+            models: [],
+            components: [],
+        }; // 文件对象，包含index.js map.js model.js等
+        files.forEach((filename) => {
+            const filedir = path.join(filePath, filename); // 绝对路径
+            const stats = fs.statSync(filedir);
+            const isFile = stats.isFile(); // 是文件
+            const isDir = stats.isDirectory(); // 是文件夹
+            if (isDir) { // 判断是文件夹
+                if (filename === 'models') { // 判断是models文件夹，遍历该文件夹
+                    const models = fs.readdirSync(filedir);
+                    models.forEach(item => {
+                        filesObj.models.push(path.join(filedir, item));
+                    });
+                } else if (filename === 'components') { // 判断是components文件夹，遍历该文件夹
+                    const components = fs.readdirSync(filedir);
+                    components.forEach(item => {
+                        filesObj.components.push(path.join(filedir, item));
+                    });
+                } else {
+                    fileDisplayDeep(filedir); // 递归，如果是文件夹，就继续遍历该文件夹下面的文件
+                }
+            }
+            if (isFile) { // 判断是文件
+                if (/index\.jsx?/.test(filename)) { // 判断是index.js 或是 index.jsx
+                    filesObj.index = filedir;
+                }
+                if (/model\.js/.test(filename)) { // 判断是model.js
+                    filesObj.models.push(filedir);
+                }
+                if (/map\.js/.test(filename)) { // 判断是map.js
+                    filesObj.map = filedir;
+                }
+            }
+        });
+        if (filesObj.index) { // 判断是否有页面
+            umifiles.push(filesObj);
+        }
+    }
+    fileDisplayDeep(filePaths);
+    return umifiles;
 };
--- src/umi/index.js
+++ src/umi/index.js
@@ -2,15 +2,15 @@ const resolve = require('../common/resolve');
 const fileDisplay = require('./file-display');
 
 module.exports = function createUmiDocument(options) {
-  let umiFiles = [];
+    let umiFiles = [];
 
-  if (options.target.length > 0) {
-  // 配置过目标文件则解析目标文件
-    for (let i = 0; i < options.target.length; i++) {
-      umiFiles = umiFiles.concat(fileDisplay(resolve(options.target[i]), resolve(options.ignore)));
+    if (options.target.length > 0) {
+    // 配置过目标文件则解析目标文件
+        for (let i = 0; i < options.target.length; i++) {
+            umiFiles = umiFiles.concat(fileDisplay(resolve(options.target[i]), resolve(options.ignore)));
+        }
+    } else {
+        umiFiles = umiFiles.concat(fileDisplay(resolve(options.entry), resolve(options.ignore)));
     }
-  } else {
-    umiFiles = umiFiles.concat(fileDisplay(resolve(options.entry), resolve(options.ignore)));
-  }
-  console.log(umiFiles);
+    // console.log(umiFiles);
 };
