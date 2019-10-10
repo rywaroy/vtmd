3ab875c0dc91aa945b757479eb2dbf3f677b551b
张志豪 <464485682@qq.com>
2019-10-10 11:15:25 +0800

    Feat: 文件夹递归添加models文件判断逻辑

--- src/umi/file-display.js
+++ src/umi/file-display.js
@@ -3,19 +3,33 @@ const path = require('path');
 
 module.exports = function fileDisplay(filePaths, ignorePaths) {
   const umifiles = [];
-  const stack = []; // 路由栈
   function fileDisplayDeep(filePath) {
     if (ignorePaths && ignorePaths.includes(filePath)) {
       return;
     }
     const files = fs.readdirSync(filePath);
+    const filesObj = {
+      index: '',
+      map: '',
+      models: [],
+      components: [],
+    }; // 文件对象，包含index.js map.js model.js等
     files.forEach((filename) => {
       const filedir = path.join(filePath, filename); // 绝对路径
       const stats = fs.statSync(filedir);
       const isFile = stats.isFile(); // 是文件
       const isDir = stats.isDirectory(); // 是文件夹
       if (isDir) {
-        fileDisplayDeep(filedir); // 递归，如果是文件夹，就继续遍历该文件夹下面的文件
+        if (filename === 'models') { // 判断是models文件夹，遍历该文件夹
+          const models = fs.readdirSync(filedir);
+          models.forEach(item => {
+            filesObj.models.push(path.join(filedir, item));
+          });
+        } else if (filename === 'components') {
+
+        } else {
+          fileDisplayDeep(filedir); // 递归，如果是文件夹，就继续遍历该文件夹下面的文件
+        }
       }
     });
   }
