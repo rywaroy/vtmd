c5a57f136cca8ef9fb2772c7601dad10d17b3852
张志豪 <464485682@qq.com>
2019-10-11 11:06:52 +0800

    Feat: 添加文件解析

--- src/umi/index.js
+++ src/umi/index.js
@@ -1,5 +1,6 @@
 const resolve = require('../common/resolve');
 const fileDisplay = require('./file-display');
+const umiAstParse = require('./umi-ast-parse');
 
 module.exports = function createUmiDocument(options) {
     let umiFiles = [];
@@ -7,10 +8,13 @@ module.exports = function createUmiDocument(options) {
     if (options.target.length > 0) {
     // 配置过目标文件则解析目标文件
         for (let i = 0; i < options.target.length; i++) {
-            umiFiles = umiFiles.concat(fileDisplay(resolve(options.target[i]), resolve(options.ignore)));
+            umiFiles = umiFiles.concat(fileDisplay(resolve(options.target[i]), resolve(options.ignore), options.target[i]));
         }
     } else {
-        umiFiles = umiFiles.concat(fileDisplay(resolve(options.entry), resolve(options.ignore)));
+        umiFiles = umiFiles.concat(fileDisplay(resolve(options.entry), resolve(options.ignore), options.entry));
     }
-    // console.log(umiFiles);
+    umiFiles.forEach(file => {
+        const notes = umiAstParse(file);
+        console.log(notes);
+    });
 };
