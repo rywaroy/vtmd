71731aa1f972f15f57793c34871f5ff86a4aa9aa
张志豪 <464485682@qq.com>
2019-10-11 10:05:19 +0800

    feat: eslint

--- src/vue/index.js
+++ src/vue/index.js
@@ -7,33 +7,33 @@ const resolve = require('../common/resolve');
 
 
 module.exports = function createVueDocument(options) {
-  let vueFiles;
+    let vueFiles;
 
-  if (options.target.length > 0) {
-  // 配置过目标文件则解析目标文件
-    const files = [];
-    for (let i = 0; i < options.target.length; i++) {
-      if (path.extname(options.target[i]) !== '.vue') {
-        options.target[i] = `${options.target[i]}.vue`;
-      }
-      files[i] = {};
-      files[i].filedir = resolve(options.target[i]);
-      files[i].filename = path.basename(files[i].filedir);
+    if (options.target.length > 0) {
+        // 配置过目标文件则解析目标文件
+        const files = [];
+        for (let i = 0; i < options.target.length; i++) {
+            if (path.extname(options.target[i]) !== '.vue') {
+                options.target[i] = `${options.target[i]}.vue`;
+            }
+            files[i] = {};
+            files[i].filedir = resolve(options.target[i]);
+            files[i].filename = path.basename(files[i].filedir);
+        }
+        vueFiles = files;
+    } else {
+        // 遍历查找所有vue文件夹
+        vueFiles = fileDisplay(resolve(options.entry), resolve(options.ignore));
     }
-    vueFiles = files;
-  } else {
-  // 遍历查找所有vue文件夹
-    vueFiles = fileDisplay(resolve(options.entry), resolve(options.ignore));
-  }
 
-  vueFiles.forEach((file) => {
-  // 解析vue文件
-    const cs = vueTemplateCompiler(file.filedir);
+    vueFiles.forEach((file) => {
+        // 解析vue文件
+        const cs = vueTemplateCompiler(file.filedir);
 
-    // 解析ast，获取注释
-    const notes = astParse(cs);
+        // 解析ast，获取注释
+        const notes = astParse(cs);
 
-    // 创建md文件
-    create(notes, file.filename, options);
-  });
+        // 创建md文件
+        create(notes, file.filename, options);
+    });
 };
--- src/vue/vue-template-compiler.js
+++ src/vue/vue-template-compiler.js
@@ -2,8 +2,8 @@ const compiler = require('vue-template-compiler');
 const fs = require('fs');
 
 module.exports = function vueTemplateCompiler(filePath) {
-  const file = fs.readFileSync(filePath, 'utf-8');
-  const parsedComponent = compiler.parseComponent(file);
-  const scriptContent = parsedComponent.script ? parsedComponent.script.content : '';
-  return scriptContent;
+    const file = fs.readFileSync(filePath, 'utf-8');
+    const parsedComponent = compiler.parseComponent(file);
+    const scriptContent = parsedComponent.script ? parsedComponent.script.content : '';
+    return scriptContent;
 };
