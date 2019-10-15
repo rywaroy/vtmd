0a0107d04d3b918f816099c6389e51ede57f8aca
张志豪 <464485682@qq.com>
2019-10-15 09:31:57 +0800

    Feat: 获取data/state功能提取到公共模块

--- src/vue/ast-parse.js
+++ src/vue/ast-parse.js
@@ -1,6 +1,7 @@
 const babelParser = require('@babel/parser');
 const traverse = require('@babel/traverse').default;
 const filterComment = require('../common/comment-parse');
+const getData = require('../common/parse-data');
 
 module.exports = function astParse(script) {
     const notes = {};
@@ -103,68 +104,6 @@ function getProps(obj) {
         }
         return props;
     }
-}
-
-/**
- * 获取data内容
- * @param {Array} props
- * @returns {Array}
- */
-function getData(props) {
-    const arr = [];
-    for (let i = 0; i < props.length; i++) {
-        const obj = {
-            name: props[i].key.name,
-        };
-        // 最后一项取trailingComments内容
-        if (i === props.length - 1) {
-            if (props[i].trailingComments) {
-                obj.value = filterComment(props[i].trailingComments);
-            }
-        } else { // 不是最后一项取下一项的leadingComments内容
-            if (props[i + 1].leadingComments) {
-                obj.value = filterComment(props[i + 1].leadingComments);
-            }
-        }
-        if (props[i].value.type === 'NumericLiteral') {
-            obj.type = 'Number';
-        }
-        if (props[i].value.type === 'StringLiteral') {
-            obj.type = 'String';
-        }
-        if (props[i].value.type === 'ArrayExpression') {
-            obj.type = 'Array';
-        }
-        if (props[i].value.type === 'ObjectExpression') {
-            obj.type = 'Object';
-            /**
-       * @example
-       * a: { // 判断这种情况的注释
-       *  b: 1,
-       * }
-       */
-            if (props[i].value.properties.length > 0) {
-                if (props[i].value.properties[0].leadingComments) {
-                    obj.value = filterComment(props[i].value.properties[0].leadingComments);
-                }
-            } else {
-                /**
-       * @example
-       * a: { // 判断这种情况的注释
-        *
-        * }
-        */
-                if (props[i].value.innerComments) {
-                    obj.value = filterComment(props[i].value.innerComments);
-                }
-            }
-        }
-        if (props[i].value.type === 'NullLiteral') {
-            obj.type = 'Any';
-        }
-        arr.push(obj);
-    }
-    return arr;
 }
 
 /**
