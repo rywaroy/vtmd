de78aee0a713afe7ede267bef0c08508231a504e
张志豪 <464485682@qq.com>
2019-10-16 17:19:16 +0800

    fix: eslint

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const createUmiDocument = require('./src/umi');
 const resolve = require('./src/common/resolve');
 
 program
-  .version('2.0.0')
-  .option('--config <path>', 'config')
-  .option('--component', 'component')
-  .option('--umi', 'umi');
+    .version('2.0.0')
+    .option('--config <path>', 'config')
+    .option('--component', 'component')
+    .option('--umi', 'umi');
 
 
 program.parse(process.argv);
@@ -20,55 +20,55 @@ let configPath;
 
 // 默认配置
 const optionsDefault = {
-  entry: 'src',
-  compontent: false,
-  ignore: null,
-  output: 'docs',
-  target: [],
+    entry: 'src',
+    compontent: false,
+    ignore: null,
+    output: 'docs',
+    target: [],
 };
 
 // 获取参数
 if (program.args.length > 0) {
-  optionsDefault.target = program.args;
+    optionsDefault.target = program.args;
 }
 
 // 判断是否组件模式
 if (program.component) {
-  optionsDefault.compontent = true;
+    optionsDefault.compontent = true;
 
-  // 创建vue组件
-  createVueComponent();
+    // 创建vue组件
+    createVueComponent();
 }
 
 // 判断是否设置自定义配置
 if (program.config) {
-  configPath = resolve(program.config);
+    configPath = resolve(program.config);
 } else {
-  // 未设置定义配置则默认使用当前目录下vtmd.config.js
-  configPath = resolve('vtmd.config.js');
+    // 未设置定义配置则默认使用当前目录下vtmd.config.js
+    configPath = resolve('vtmd.config.js');
 }
 
 // 判断配置文件是否存在
 const exists = fs.existsSync(configPath);
 let options;
 if (exists) {
-  // 存在配置文件则合并配置
-  const configOptions = require(configPath);
-  options = {
-    ...optionsDefault,
-    ...configOptions,
-  };
+    // 存在配置文件则合并配置
+    const configOptions = require(configPath);
+    options = {
+        ...optionsDefault,
+        ...configOptions,
+    };
 } else {
-  // 不存在配置文件则使用默认配置
-  options = optionsDefault;
+    // 不存在配置文件则使用默认配置
+    options = optionsDefault;
 }
 
 if (typeof options.ignore === 'string') {
-  options.ignore = [options.ignore];
+    options.ignore = [options.ignore];
 }
 
 if (program.umi) {
-  createUmiDocument(options);
+    createUmiDocument(options);
 } else {
-  createVueDocument(options);
+    createVueDocument(options);
 }
