eec0fd8cb125fe6ed3bc52a7da361c2e073a9542
张志豪 <464485682@qq.com>
2019-11-13 15:03:26 +0800

    feat: 解析model 的state

--- src/umi/model-parse.js
+++ src/umi/model-parse.js
@@ -1,5 +1,6 @@
 const traverse = require('@babel/traverse').default;
 const filterComment = require('../common/comment-parse');
+const dataParse = require('../common/data-parse');
 
 /**
  * 解析 map.js 文件
@@ -11,11 +12,27 @@ module.exports = function modelParse(ast) {
         ExportDefaultDeclaration(path) {
             const properties = path.node.declaration.properties;
             properties.forEach(item => {
+                // 获取namespace
                 if (item.key.name === 'namespace') {
                     model.namespace = item.value.value;
+                }
+                // 获取state
+                if (item.key.name === 'state') {
+                    /**
+                     * 判断是对象
+                     * @example
+                     * state: {
+                     *  a: 1,
+                     *  b: 2,
+                     * }
+                     */
+                    if (item.value.type === 'ObjectExpression') {
+                        model.state = dataParse(item.value.properties);
+                    }
                 }
             });
         },
     });
+    console.log(JSON.stringify(model));
     return model;
 };
