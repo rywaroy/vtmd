f16a27b27b7ecf35e632fd6ecd27385ce18021db
张志豪 <464485682@qq.com>
2019-11-14 09:12:42 +0800

    Feat: 添加进度条展示

--- src/umi/index.js
+++ src/umi/index.js
@@ -1,6 +1,9 @@
+const log = require('single-line-log').stdout;
+require('colors');
 const resolve = require('../common/resolve');
 const fileDisplay = require('./file-display');
 const umiAstParse = require('./umi-ast-parse');
+
 
 module.exports = function createUmiDocument(options) {
     let umiFiles = [];
@@ -13,8 +16,30 @@ module.exports = function createUmiDocument(options) {
     } else {
         umiFiles = umiFiles.concat(fileDisplay(resolve(options.entry), resolve(options.ignore), options.entry));
     }
+    const total = umiFiles.length;
+    let num = 0;
     umiFiles.forEach(file => {
+        creatProgress(num, total, `正在解析${file.baseUrl}`);
         const notes = umiAstParse(file);
+        num++;
+        creatProgress(num, total, `${file.baseUrl} 解析完成`);
         // console.log(notes);
     });
+    creatProgress(num, total, '解析完成!');
 };
+
+function creatProgress(num, total, text) {
+    const percent = num / total;
+    const done = Math.floor(20 * percent);
+    const none = 20 - done;
+    let p = '';
+    for (let i = 0; i < done; i++) {
+        p += '▇'.green;
+    }
+    for (let i = 0; i < none; i++) {
+        p += '▇'.gray;
+    }
+    log(`${text}
+${'· vtmd'.green} ${p}  ${num}/${total}
+`);
+}
