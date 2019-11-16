60d6d0ea7f8d3f16ba45425b18c77a15f0be9410
张志豪 <464485682@qq.com>
2019-11-16 13:46:33 +0800

    feat: 提取公共模块

--- src/vue/create-md.js
+++ src/vue/create-md.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const getNote = require('../common/get-note');
 const createNote = require('../common/create-note');
+const createComponentNote = require('../common/create-component-note');
 
 module.exports = function create(notes, filename, options) {
     let md;
@@ -195,60 +196,5 @@ function createComponentMethod(name, notes) {
             md += '/> \n\n';
         }
     });
-    return md;
-}
-
-/**
- * 创建注释字符串
- * @param {Object} note
- * @returns {String}
- */
-function createComponentNote(note) {
-    let md = '';
-    if (note.intro) {
-        md += `::: tip ${note.intro.cn}
-${note.intro.value}
-:::
-`;
-    }
-    md += '<vtmd-notes \n';
-    if (note.version) {
-        md += ` version="${note.version.value}"\n`;
-        md += ` versionCn="${note.version.cn}"\n`;
-    }
-    if (note.author) {
-        md += ` author="${note.author.value}"\n`;
-        md += ` authorCn="${note.author.cn}"\n`;
-    }
-    if (note.url) {
-        md += ` url="${note.url.value}"\n`;
-        md += ` urlCn="${note.url.cn}"\n`;
-    }
-    if (note.image) {
-        md += ` image="${note.image.value}"\n`;
-        md += ` imageCn="${note.image.cn}"\n`;
-    }
-    if (note.txt) {
-        md += ` txt="${note.txt.value}"\n`;
-    }
-    md += '/> \n\n';
-    if (note.param) {
-        md += '**参数** \n\n';
-        md += `| 参数 | 类型 | 说明
-| ---- | ---- | ---- | \n`;
-        note.param.forEach(item => {
-            md += `| ${item.param} | ${item.type} | ${item.value} \n`;
-        });
-        md += '\n';
-    }
-    if (note.returns) {
-        md += '**返回值** \n\n';
-        md += `| 类型 | 说明
-| ---- | ---- | \n`;
-        note.returns.forEach(item => {
-            md += `| ${item.type} | ${item.value} \n`;
-        });
-        md += '\n';
-    }
     return md;
 }
